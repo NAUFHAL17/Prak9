51f129af78fd42c81610c55a5635c534332c0530
NAUFHAL17 <naufhalardyansyah5@gmail.com>
2023-07-12 17:12:49 +0700

    uas pbo

--- Main.java
+++ Main.java
@@ -1,29 +1,57 @@
 public class Main {
+    public static void main(String[] args) {
+ 
+        Buku buku1 = new Buku("B001", "Pemrograman Java");
+        Buku buku2 = new Buku("B002", "Pemrograman C++");
+        Buku buku3 = new Buku("B003", "Harry Potter");
+ 
+        Petugas petugas1 = new Petugas("Naufhal", "P001");
+        Peminjam peminjam1 = new Mahasiswa();
+        Peminjam peminjam2 = new Mahasiswa();
+        peminjam1.setNamaPeminjam("Naufhal");
+        peminjam1.setKodePeminjam("M001");
+        peminjam2.setNamaPeminjam("Ardyansyah");
+        peminjam2.setKodePeminjam("M001");
+        
+        peminjam1.doLogin();
+        peminjam2.doLogin();
+        
+        try{
+        Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku1);
+        Peminjaman peminjaman2 = new Peminjaman(peminjam2, petugas1, buku3);
 
-public static void main(String[] args) {
-    Buku buku1 = new Buku("B001", "Pemrograman Java"); 
-    Buku buku2 = new Buku("B002", "Pemrograman C++");
-    Buku buku3 = new Buku("B003", "Harry Potter");
+        System.out.println("Peminjaman: " + peminjaman1.getPeminjam().getNamaPeminjam());
+        System.out.println("Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
+        System.out.println("Buku: " + peminjaman1.getBuku().getNamaBuku());
+        System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku() );
+        Denda denda1 = new Denda(peminjaman1, 5);
+        System.out.println("Tagihan denda: " + denda1.getTagihan());
 
-Petugas petugas1 = new Petugas("Alex", "P001"); 
-Peminjam peminjam1 = new Mahasiswa();
-peminjam1.setNamaPeminjam("Budi"); 
-peminjam1.setKodePeminjam("M001");
+        System.out.println("===================================>");
+        
+        System.out.println("Peminjaman: " + peminjaman2.getPeminjam().getNamaPeminjam());
+        System.out.println("Petugas: " + peminjaman2.getPetugas().getNamaPetugas());
+        System.out.println("Buku: " + peminjaman2.getBuku().getNamaBuku());
+        System.out.println("Nama Buku: " + peminjaman2.getBuku().getNamaBuku() );
+        Denda denda2 = new Denda(peminjaman2, 7);
+        System.out.println("Tagihan denda: " + denda2.getTagihan());
 
-peminjam1.doLogin();
-//peminjam1.doLogout();
+        System.out.println("=== Denda dihapuskan ! ===");
 
-try{
-Peminjaman peminjaman1 = new Peminjaman(peminjam1, petugas1, buku1);
-System.out.println("Peminjaman: " + peminjaman1.getPeminjam().getNamaPeminjam()); 
-System.out.println("Petugas: " + peminjaman1.getPetugas().getNamaPetugas());
-System.out.println("Buku: " + peminjaman1.getBuku().getNamaBuku());
-System.out.println("Nama Buku: " + peminjaman1.getBuku().getNamaBuku() );
-} catch (Exception e) {
-System.out.println("terjadi kesalahan: " + e.getMessage());
-} finally {
-    System.out.println("Pemograman Selesai");
-}
+        peminjam2.addAntiDenda();
 
-}
+        System.out.println("Tagihan denda: " + denda2.getTagihan());
+
+        System.out.println("=== Denda ditambahkan ! ===");
+
+        peminjam2.removeAntiDenda();
+
+        System.out.println("Tagihan denda: " + denda2.getHapus());
+
+    } catch (Exception e) {
+        System.out.println("terjadi kesalahan: " + e.getMessage());
+    } finally {
+            System.out.println("Pemograman Selesai");
+        }  
+    }
 }
